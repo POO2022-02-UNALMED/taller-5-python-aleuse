ecadb178fe8d4fb3bd3eac1fc5b2549938a6dde2
aleuse <angalvish@unal.edu.co>
2022-09-20 21:03:26 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -53,11 +53,11 @@ class Animal:
 
     @staticmethod
     def totalPorTipo():
-        cadena = (f"Mamiferos: {len(zooAnimales.mamifero.Mamifero.getListado())} \nAves: {len(zooAnimales.ave.Ave.getListado())} \nReptiles: {len(zooAnimales.reptil.Reptil.getListado())} \nPeces: {len(zooAnimales.pezPez.getListado())} \nAnfibios {len(zooAnimales.anfibio.Anfibio.getListado())}")
+        cadena = (f"Mamiferos: {len(zooAnimales.mamifero.Mamifero.getListado())} \nAves: {len(zooAnimales.ave.Ave.getListado())} \nReptiles: {len(zooAnimales.reptil.Reptil.getListado())} \nPeces: {len(zooAnimales.pez.Pez.getListado())} \nAnfibios {len(zooAnimales.anfibio.Anfibio.getListado())}")
         return cadena
 
     def toString(self):
-        if len(self.getZona()) != 0:
+        if self.getZona() != None:
             return (f"Mi nombre es {self.getNombre()}, tengo una edad de {self.getEdad()}, habito en {self.getHabitat()} y mi genero es {self.getGenero()}, la zona en la que me ubico es {self.getZona()}, en el zoo {self.getZona().getZoo().getNombre()}")
         else:
             return (f"Mi nombre es {self.getNombre()}, tengo una edad de {self.getEdad()}, habito en {self.getHabitat()} y mi genero es {self.getGenero()}")
